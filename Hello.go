6d94d180d7aefc0c706de0c7440c47c973905095
Azri Rahmat <azri-rahmat@Azris-MacBook-Pro.local>
2022-03-28 12:14:41 +0800

    Adding 'Hi Earth' into codebase

--- Hello.go
+++ Hello.go
@@ -1,12 +1,16 @@
 package main
 
+import "fmt"
+
 func HelloWorld() string {
 	//Insert your code here
-
+	messageHelloWorld := "Hello World from Go !!"
+	fmt.Println(messageHelloWorld)
 	//Do not remove this line here
 	return messageHelloWorld
 }
 
 func main() {
 	HelloWorld()
+	fmt.Println("Hi Earth")
 }
